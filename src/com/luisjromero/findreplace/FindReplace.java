c76f30706887b408af3ac12ab30105d9a16660ba
Luis J. Romero <romeroluisj@gmail.com>
2020-09-11 21:33:50 -0400

    Added BufferedReader and test file

--- src/com/luisjromero/findreplace/FindReplace.java
+++ src/com/luisjromero/findreplace/FindReplace.java
@@ -1,7 +1,30 @@
 package com.luisjromero.findreplace;
 
-public class FindReplace {
-    public static void main(String[] args) {
-        System.out.println("Test");
+import java.io.*;
+
+public class FindReplace
+{
+    public static void main(String args[])
+    {
+        try
+        {
+            File file=new File("Demo.txt");    //creates a new file instance
+            FileReader fr=new FileReader(file);   //reads the file
+            BufferedReader br=new BufferedReader(fr);  //creates a buffering character input stream
+            StringBuffer sb=new StringBuffer();    //constructs a string buffer with no characters
+            String line;
+            while((line=br.readLine())!=null)
+            {
+                sb.append(line);      //appends line to string buffer
+                sb.append("\n");     //line feed
+            }
+            fr.close();    //closes the stream and release the resources
+            System.out.println("Contents of File: ");
+            System.out.println(sb.toString());   //returns a string that textually represents the object
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
